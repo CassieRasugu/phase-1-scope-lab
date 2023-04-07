6cba1480761ee3354de972012b5450c5b4fdaf68
CassieRasugu <cassierasugu@gmail.com>
2023-04-07 22:53:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+// Define function overwriteBestCustomer()
+function overwriteBestCustomer() {
+    bestCustomer = "new value";
+}
+
+// Declare constant leastFavoriteCustomer using const
+const leastFavoriteCustomer = "Nigel";
+
+// Define function changeLeastFavoriteCustomer()//
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "Cassie"; 
+}
+console.log("Initial customerName:", customerName);
+upperCaseCustomerName();
+console.log("After calling upperCaseCustomerName():", customerName);
+
+console.log("Initial bestCustomer:", bestCustomer);  
+setBestCustomer();
+console.log("After calling setBestCustomer():", bestCustomer);
+
+console.log("Initial leastFavoriteCustomer:", leastFavoriteCustomer);
+changeLeastFavoriteCustomer(); 
+console.log("After calling changeLeastFavoriteCustomer():", leastFavoriteCustomer);
